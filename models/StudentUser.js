fe985f389dbfee8ee19114967af5081899ae90ff
vatsasaumya <59758451+vatsasaumya@users.noreply.github.com>
2020-02-09 01:11:49 +0530

    Add files via upload

---
+++ models/StudentUser.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose= require("passport-local-mongoose");
+
+var StudentUserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+
+    },
+    password:{
+        type: String,
+        required: true
+
+    },
+    student_id:{
+        type:String,
+        required:true
+    },
+    phone_no:{
+        type: String,
+        required: true
+
+    },
+    dob:{
+        type: Date,
+        default: Date.now
+
+    },
+    school:{
+        type: String,
+        required: true
+    },
+    batch:{
+        type: String,
+        required: true
+    }
+});
+StudentUserSchema.plugin(passportLocalMongoose);
+module.exports = User =  mongoose.model("User",StudentUserSchema);
